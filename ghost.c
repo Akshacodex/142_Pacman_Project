05fcdce25bad94701512a049947a6148e6bed038
Akshacodex <this.letchu@gmail.com>
2024-04-06 23:47:19 -0400

    Final edited and comment version that will be used to submit.

--- ghost.c
+++ ghost.c
@@ -15,13 +15,15 @@ char sees_pacman(int pacman_y, int pacman_x, int ghost_y, int ghost_x) {
     //if the ghost and pacman are in the same position, then pacman will be eaten
     if (ghost_x == pacman_x && ghost_y == pacman_y) {
         return EATING_PACMAN;
-    } else if (ghost_y == pacman_y && abs(ghost_x - pacman_x) > 0) { //else if pacman and the ghost are on the same row but different columns
+    } else if (ghost_y == pacman_y &&
+               abs(ghost_x - pacman_x) > 0) { //else if pacman and the ghost are on the same row but different columns
         if (pacman_x > ghost_x) { //if pacman is to the right of the ghost
             see_direction = RIGHT; //the ghost looks to the right
         } else {
             see_direction = LEFT; //or else, the ghost looks to the left
         }
-    } else if (pacman_x == ghost_x && abs(ghost_y - pacman_y) > 0) { //else if pacman and the ghost are on the same column but different rows
+    } else if (pacman_x == ghost_x &&
+               abs(ghost_y - pacman_y) > 0) { //else if pacman and the ghost are on the same column but different rows
         if (pacman_y > ghost_y) { //if pacman is under the ghost
             see_direction = DOWN; //the ghost looks down
         } else {
@@ -32,28 +34,32 @@ char sees_pacman(int pacman_y, int pacman_x, int ghost_y, int ghost_x) {
     if (see_direction != SEES_NOTHING) { //if the ghost sees pacman
         switch (see_direction) { //there are 4 possibilities when the ghost sees pacman
             case UP: //when the ghost sees pacman on top of itself
-                for (int i = 1; i < abs(ghost_y - pacman_y); i++) { //loop counter counts the space between the ghost and pacman
+                for (int i = 1;
+                     i < abs(ghost_y - pacman_y); i++) { //loop counter counts the space between the ghost and pacman
                     if (!is_wall(pacman_y + i, pacman_x)) { //if there isn't a wall then it will look up
                         return see_direction;
                     }
                 }
                 break;
             case DOWN: //when the ghost sees pacman below itself
-                for (int i = 1; i < abs(ghost_y - pacman_y); i++) {  //loop counter counts the space between the ghost and pacman
+                for (int i = 1;
+                     i < abs(ghost_y - pacman_y); i++) {  //loop counter counts the space between the ghost and pacman
                     if (!is_wall(pacman_y - i, pacman_x)) { //if there isn't a wall then it will look down
                         return see_direction;
                     }
                 }
                 break;
             case LEFT: //when the ghost sees pacman to the left of itself
-                for (int i = 1; i < abs(ghost_x - pacman_x); i++) { //loop counter counts the space between the ghost and pacman
+                for (int i = 1;
+                     i < abs(ghost_x - pacman_x); i++) { //loop counter counts the space between the ghost and pacman
                     if (!is_wall(pacman_y, pacman_x + i)) { //if there isn't a wall then it will look to the left
                         return see_direction;
                     }
                 }
                 break;
             case RIGHT: //when the ghost sees pacman to the right of itself
-                for (int i = 1; i < abs(ghost_x - pacman_x); i++) { //loop counter counts the space between the ghost and pacman
+                for (int i = 1;
+                     i < abs(ghost_x - pacman_x); i++) { //loop counter counts the space between the ghost and pacman
                     if (!is_wall(pacman_y, pacman_x - i)) { //if there isn't a wall then it will look to the right
                         return see_direction;
                     }
@@ -63,3 +69,4 @@ char sees_pacman(int pacman_y, int pacman_x, int ghost_y, int ghost_x) {
     }
 
     return MOVE_RANDOMLY;
+}
